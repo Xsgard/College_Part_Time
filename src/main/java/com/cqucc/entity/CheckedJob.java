58ad36e763994388129caf56f4b7dd804eb6ca29
asgard <1792181243@qq.com>
2023-02-18 23:04:54 +0800

    实体类：CheckedJob

---
+++ src/main/java/com/cqucc/entity/CheckedJob.java
@@ -0,0 +1,41 @@
+package com.cqucc.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+public class CheckedJob implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long userId;
+
+    private Long jobId;
+
+    private String jobName;
+
+    private String companyName;
+
+    //创建时间
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+
+    //更新时间
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    //创建人
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+
+    //修改人
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+}
