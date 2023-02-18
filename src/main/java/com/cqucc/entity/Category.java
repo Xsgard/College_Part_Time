308f63184a10117ef2e72e6b9dcfc9398e185512
asgard <1792181243@qq.com>
2023-02-18 23:01:11 +0800

    实体类：Category

---
+++ src/main/java/com/cqucc/entity/Category.java
@@ -0,0 +1,38 @@
+package com.cqucc.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Category {
+    //分类ID
+    private Long id;
+
+    //分类名
+    private String name;
+
+    //排序
+    private Integer sort;
+
+    //创建时间
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+
+    //更新时间
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    //创建人
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+
+    //修改人
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+}
