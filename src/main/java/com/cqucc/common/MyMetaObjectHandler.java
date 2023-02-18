41192e8bd53925168a01ce1a5877b6165d23403a
asgard <1792181243@qq.com>
2023-02-18 21:32:18 +0800

    common包类资源

--- src/main/java/com/cqucc/common/MyMetaObjectHandler.java
+++ src/main/java/com/cqucc/common/MyMetaObjectHandler.java
@@ -18,16 +18,16 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     public void insertFill(MetaObject metaObject) {
         log.info("公告字段自动填充[insert]...");
         log.info(metaObject.toString());
-        metaObject.setValue("createTime", LocalDateTime.now());
+//        metaObject.setValue("createTime", LocalDateTime.now());
         metaObject.setValue("updateTime", LocalDateTime.now());
-        metaObject.setValue("createUser", BaseContext.getCurrentId());
-        metaObject.setValue("updateUser", BaseContext.getCurrentId());
+//        metaObject.setValue("createUser", BaseContext.getCurrentId());
+//        metaObject.setValue("updateUser", BaseContext.getCurrentId());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("公告字段自动填充[update]...");
         metaObject.setValue("updateTime", LocalDateTime.now());
-        metaObject.setValue("updateUser", BaseContext.getCurrentId());
+//        metaObject.setValue("updateUser", BaseContext.getCurrentId());
     }
 }
