cac866744f3a32b1ffc98550a44be6d9e47c63bf
asgard <1792181243@qq.com>
2023-02-18 23:04:29 +0800

    分类Service

---
+++ src/main/java/com/cqucc/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.cqucc.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.cqucc.entity.Category;
+
+public interface CategoryService extends IService<Category> {
+}
