89e5369b556f24de9497330fccaaa9b1acbdda76
asgard <1792181243@qq.com>
2023-02-23 00:16:50 +0800

    启动类

--- src/main/java/com/cqucc/CollegePartTimeApplication.java
+++ src/main/java/com/cqucc/CollegePartTimeApplication.java
@@ -1,13 +1,16 @@
 package com.cqucc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CollegePartTimeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CollegePartTimeApplication.class, args);
+        log.info("项目启动成功...");
     }
 
 }
