e9013860e8dd9e56fb92f3205301b5be7fc34bc7
asgard <1792181243@qq.com>
2023-02-22 14:36:59 +0800

    用户Controller

--- src/main/java/com/cqucc/controller/UserController.java
+++ src/main/java/com/cqucc/controller/UserController.java
@@ -1,18 +1,19 @@
 package com.cqucc.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cqucc.common.R;
 import com.cqucc.entity.User;
 import com.cqucc.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -20,6 +21,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 用户登录
+     *
+     * @param request
+     * @param user
+     * @return
+     */
     @PostMapping("/login")
     public R<User> login(HttpServletRequest request, @RequestBody User user) {
         //1.将页面提交的密码password进行md5加密
@@ -47,15 +55,29 @@ public class UserController {
         return R.success(one);
     }
 
+    /**
+     * 用户退出
+     *
+     * @param request
+     * @return
+     */
     @PostMapping("/logout")
     public R<String> logout(HttpServletRequest request) {
         request.getSession().removeAttribute("user");
         return R.success("登出成功！");
     }
 
+    /**
+     * 用户注册
+     *
+     * @param request
+     * @param user
+     * @return
+     */
     @PostMapping("/register")
     public R<String> register(HttpServletRequest request, @RequestBody User user) {
         user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes(StandardCharsets.UTF_8)));
+        user.setImage("d562e08f-e0fd-49be-a5b8-0b66948dfecc.jpeg");
 
         if (user.getLicense() != null) {
             user.setIdentity(2);
@@ -66,4 +88,37 @@ public class UserController {
         userService.save(user);
         return R.success("注册成功！");
     }
+
+    /**
+     * 根据ID获取用户信息
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/list/{id}")
+    public R<Page<User>> getUserList(@PathVariable Long id) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getId, id);
+        User user = userService.getOne(queryWrapper);
+        Page p = new Page(1, 1);
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        p.setRecords(userList);
+
+        return R.success(p);
+    }
+
+
+    /**
+     * 修改用户信息
+     *
+     * @param user
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody User user) {
+        userService.updateById(user);
+        return R.success("修改成功！");
+    }
+
 }
