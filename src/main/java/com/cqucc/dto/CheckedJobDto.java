2f9c4f2ffc958b0ee2482df5aeedfc0ecd44bee0
asgard <1792181243@qq.com>
2023-02-27 18:11:56 +0800

    已选择兼职Dto类

--- src/main/java/com/cqucc/dto/CheckedJobDto.java
+++ src/main/java/com/cqucc/dto/CheckedJobDto.java
@@ -5,10 +5,16 @@ import lombok.Data;
 
 @Data
 public class CheckedJobDto extends Job {
+
+    //用户ID
     private Long userId;
-
+    //电话号码
     private String phone;
-
+    //邮箱
     private String email;
+    //地点
+    private String location;
+    //描述
+    private String description;
 
 }
