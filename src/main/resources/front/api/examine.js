c837a3b0867f6dce6cc79fe4d7c90df096237661
asgard <1792181243@qq.com>
2023-03-01 14:30:04 +0800

    资源文件

--- src/main/resources/front/api/examine.js
+++ src/main/resources/front/api/examine.js
@@ -1,7 +1,19 @@
-const getExaminePage=(param)=>{
+const getExaminePage = (param) => {
     return $axios({
-        url:'/job',
-        method:'get',
-        param
+        url: '/user/page',
+        method: 'post',
+        data: {...param}
+    })
+}
+const passCom = (ids) => {
+    return $axios({
+        url: `/user/passCom/${ids}`,
+        method: 'get'
+    })
+}
+const passJob = (ids) => {
+    return $axios({
+        url: `/user/passJob/${ids}`,
+        method: 'get'
     })
 }
