0bca03bb66c7fc5645185853ed1168d8a41ec677
asgard <1792181243@qq.com>
2023-04-27 22:56:00 +0800

    论坛js文件

--- src/main/resources/front/api/forum.js
+++ src/main/resources/front/api/forum.js
@@ -6,4 +6,36 @@ const getPostPage = (params) => {
         params
     })
 }
+const getPost = (id) => {
+    return $axios({
+        url: `/forum/${id}`,
+        method: 'get'
+    })
+}
+
+//  新增接口
+const addPost = (params) => {
+    return $axios({
+        url: '/forum',
+        method: 'post',
+        data: {...params}
+    })
+}
+
+const submitRateValue = (postRate) => {
+    return $axios({
+        url: '/forum/rateValue',
+        method: 'put',
+        data: {...postRate}
+    })
+}
+
+const addComment = (params) => {
+    return $axios({
+        url: '/forum/addComment',
+        method: 'post',
+        data: {...params}
+    })
+}
+
 
